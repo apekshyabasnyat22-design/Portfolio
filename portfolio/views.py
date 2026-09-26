fb52eb9b90eb05d8bd11254b3663cf1b589f7f44
Apekshya Basnyat <apekshyabasnyat22@gmail.com>
2026-09-26 21:41:40 +0545

    Add separate projects page

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ContactForm
@@ -9,17 +10,22 @@ def home(request):
     return render(
         request,
         'portfolio/home.html',
-        {
-            'projects': projects
-        }
+        {'projects': projects}
+    )
+
+
+def projects(request):
+    all_projects = Project.objects.all()
+
+    return render(
+        request,
+        'portfolio/projects.html',
+        {'projects': all_projects}
     )
 
 
 def about(request):
-    return render(
-        request,
-        'portfolio/about.html'
-    )
+    return render(request, 'portfolio/about.html')
 
 
 def contact(request):
@@ -29,21 +35,15 @@ def contact(request):
         if form.is_valid():
             form.save()
             return redirect('contact_success')
-
     else:
         form = ContactForm()
 
     return render(
         request,
         'portfolio/contact.html',
-        {
-            'form': form
-        }
+        {'form': form}
     )
 
 
 def contact_success(request):
-    return render(
-        request,
-        'portfolio/contact_success.html'
-    )
+    return render(request, 'portfolio/contact_success.html')
